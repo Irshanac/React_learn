773a4450fcae5758aaded800c784d8562efb0eac
Irshanac <irshana951@gmail.com>
2024-09-03 10:19:50 +0430

    day1

---
+++ day1.js
@@ -0,0 +1,20 @@
+let a=5
+console.log(typeof(a));
+a='asdf'
+console.log(typeof(a));
+console.log(a);
+let b=4
+console.log(typeof(b));
+console.log(b);
+b=7.8
+console.log(typeof(b));
+console.log(b);
+b=134n
+console.log(typeof(b));
+console.log(b);
+console.log(Number(3));
+console.log(Symbol("absc"));
+let c=Symbol()
+let x=null,o=9
+let g=x ?? o
+console.log(g);
